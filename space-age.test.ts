c0b072808aea10eedd7786bf8edea8802066ffd9
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 17:56:43 -0400

    Add test

--- space-age.test.ts
+++ space-age.test.ts
@@ -1,1 +1,8 @@
 import { age } from "./space-age";
+
+describe("age function tests", () => {
+    it("converts to mercury", () => {
+        const result = age('mercury', 31557600)
+        expect(result).toEqual(4.15)
+    })
+})
